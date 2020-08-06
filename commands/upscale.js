c31908b727d9ea991607f72036b77dd7d6bd6c93
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-07 00:59:37 +0200

    Update upscale.js

--- commands/upscale.js
+++ commands/upscale.js
@@ -1,6 +1,6 @@
 const {MessageEmbed} = require('discord.js');
 const imageDownloader = require('image-downloader');
-const {Waifu2x} = require('waifu2x');
+const waifu2x = require('waifu2x');
 
 module.exports = {
     name: "upscale",
@@ -9,13 +9,13 @@ module.exports = {
     usage: "<image>",
 
     async senko(client, message, args) {
-        console.log(Waifu2x)
-        if (!message.attachments) {
+        console.log(waifu2x.default)
+        if (!message.attachments.array()[0]) {
             return message.channel.send(`:( no attachments`);
         }
         var attachment = message.attachments.array()[0];
         imageDownloader.image({url: attachment.url, dest: './tmpimgs'}).then(async ({filename}) => {
-            await Waifu2x.upscaleImage(`./tmpimgs/${filename}.png`, `./tmpimgsupscaled/${filename}2x.png`, {noise: 2, scale: 2.0});
+            await waifu2x.upscaleImage(`./tmpimgs/${filename}.png`, `./tmpimgsupscaled/${filename}2x.png`, {noise: 2, scale: 2.0});
             var upscaledImage = require(`./tmpimgsupscaled/${filename}.png`);
             return message.channel.send(upscaledImage);
         });
