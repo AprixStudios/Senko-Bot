ade2f2c891230226df1604e67466992118634bfb
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-07 00:49:54 +0200

    Update upscale.js

--- commands/upscale.js
+++ commands/upscale.js
@@ -13,9 +13,9 @@ module.exports = {
             return message.channel.send(`:( no attachments`);
         }
         var attachment = message.attachments.array()[0];
-        imageDownloader.image({url: attachment.url, dest: '../tmpimgs'}).then(async ({filename}) => {
-            await waifu2x.upscaleImage(`../tmpimgs/${filename}.png`, `../tmpimgsupscaled/${filename}2x.png`);
-            var upscaledImage = require(`../tmpimgsupscaled/${filename}.png`);
+        imageDownloader.image({url: attachment.url, dest: './tmpimgs'}).then(async ({filename}) => {
+            await waifu2x.upscaleImage(`./tmpimgs/${filename}.png`, `./tmpimgsupscaled/${filename}2x.png`, {noise: 2, scale: 2.0});
+            var upscaledImage = require(`./tmpimgsupscaled/${filename}.png`);
             return message.channel.send(upscaledImage);
         });
     }
