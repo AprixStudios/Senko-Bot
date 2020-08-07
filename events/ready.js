9db9bc198608456c47eb4e827f6c8c55a5a7019e
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-07 08:57:19 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,9 @@
 module.exports = (client) => {
     console.log(`Running!`);
+    client.shard.fetchClientValues('guilds.cache.size').then(results => {
+        client.user.setPresence({ activity: { name: `Pampering ${results.reduce((prev, guildCount) => prev + guildCount, 0)} servers`}, ststus: "online"});
+        setInterval(() => {
+            client.user.setPresence({ activity: { name: `Pampering ${results.reduce((prev, guildCount) => prev + guildCount, 0)} servers`}, ststus: "online"});
+        }, 600000);
+    });
 }
