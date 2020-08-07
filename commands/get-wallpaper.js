342841c03d6a8c5f88a810c8d9308513d4d3eb00
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-07 07:16:25 +0200

    Update get-wallpaper.js

--- commands/get-wallpaper.js
+++ commands/get-wallpaper.js
@@ -1,30 +1,37 @@
-const randomAnimeWallpapers = require('random-anime-wallpapers');
+const randomAnime = require('random-anime');
 const {MessageEmbed} = require('discord.js');
 const {branding} = require('../config.json').colors;
+const {images} = require('../config.json');
 
 module.exports = {
     name: "get-wallpaper",
     description: "Get a random anime wallpaper!",
-    usage: "[keyword]",
+    usage: "[nsfw]",
     aliases: ["wallpaper"],
     category: "image",
 
     async senko(client, message, args) {
-        if (args[0]) {
-            let images = await randomAnimeWallpapers(args[0]);
+        if (args[0].search(/nsfw/i)>=0) {
+            if (message.channel.nsfw !== true) {
+                let embed = new MessageEmbed()
+                .setColor(branding)
+                .setDescription(`but.. but this isn't a NSFW channel!`)
+                .setImage(images[0])
+                return message.channel.send(embed).catch(err => err);
+            }
+            var images = randomAnime.nsfw();
             console.log(images)
             let embed = new MessageEmbed()
             .setColor(branding)
-            .setImage(images.full)
+            .setImage(images)
             return message.channel.send(embed).catch(err => err);
         } else {
-            let images = await randomAnimeWallpapers().then(images => {
-                console.log(images);
-                let embed = new MessageEmbed()
-                .setColor(branding)
-                .setImage(images.full)
-                return message.channel.send(embed).catch(err => err);
-            });
+            let images = randomAnime.anime();
+            console.log(images);
+            let embed = new MessageEmbed()
+            .setColor(branding)
+            .setImage(images)
+            return message.channel.send(embed).catch(err => err);
         }
     }
 }
